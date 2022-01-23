87cac21dc90831cd0911692c56fa2af61e2d4e4b
wanxiankai <wanxiankai@boohee.com>
2022-01-23 18:30:58 +0800

    feat: change lang back to en

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'wanxiankai@126.com',
   link: 'https://nimbus2k.vercel.app',
   description: 'This gonna be an awesome website.',
-  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
